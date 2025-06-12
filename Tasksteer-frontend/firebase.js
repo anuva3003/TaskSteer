862c13fc3e881ca62eee1fbab05b142bbbdc96c0
anuva3003 <anuvarshini.22@ichat.sp.edu.sg>
2025-06-12 15:20:23 +0800

    Update firebase.js

--- Tasksteer-frontend/firebase.js
+++ Tasksteer-frontend/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPneX8HLvkg1z6fGpTKUDDFOFUJkdf-iA",
-  authDomain: "tasksteer.firebaseapp.com",
-  projectId: "tasksteer",
-  storageBucket: "tasksteer.appspot.com",
-  messagingSenderId: "553442098430",
-  appId: "1:553442098430:web:9a79768fe81bdd15a379af",
-  measurementId: "G-WQ2MC2GJNS"
+  apiKey: "",
+  authDomain: "firebaseapp.com",
+  projectId: "",
+  storageBucket: ".appspot.com",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
